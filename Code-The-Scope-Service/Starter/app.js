806d54dd9abbb33e106d905680eaff9134cb5dbc
bvluong <bryantvinluong@gmail.com>
2017-08-15 14:01:38 -0700

    Added new messages module

--- Code-The-Scope-Service/Starter/app.js
+++ Code-The-Scope-Service/Starter/app.js
@@ -1,12 +1,22 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
-myApp.controller('mainController', function($scope) {
+myApp.controller('mainController', function($scope, $log, $filter) {
   $scope.name = 'Jane doe';
   $scope.occupation = 'Coder';
+  $scope.formattedname = $filter('uppercase')($scope.name);
+
+  console.log($scope.formattedname);
 
   $scope.getname = function() {
     return 'John Doe';
   };
-
-  console.log($scope);
+  // console.log($log);
+  // console.log($scope);
+  // console.log($filter);
 });
+
+var searchPeople = function(firstName, lastName, height, age, occupation) {
+  return 'Jane Doe';
+};
+
+console.log(angular.injector().annotate(searchPeople));
