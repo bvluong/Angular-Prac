0c4fbf53d7431e09617742c57793bb1afbbd007f
bvluong <bryantvinluong@gmail.com>
2017-08-16 16:55:39 -0700

    Add routeProvider to direct url paths

---
+++ Code-Routing-Templates-Controllers-Part-1/Starter/app.js
@@ -0,0 +1,29 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+  $routeProvider.when('/', {
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+
+  }).when('/second', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+  });
+
+});
+
+myApp.controller('mainController', ['$scope', '$location', '$log',
+  function($scope, $location, $log) {
+
+    $scope.name = 'Main';
+    $log.info($location.path());
+
+}]);
+
+
+myApp.controller('secondController', ['$scope', '$location', '$log',
+  function($scope, $location, $log) {
+    $scope.name = 'Second';
+
+}]);
