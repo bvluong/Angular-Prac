e36a99c3da0e199ac626f88b95d245b35a74decf
bvluong <bryantvinluong@gmail.com>
2017-08-15 18:00:59 -0700

    Add director to change alert based on length of input

--- Code-The-Scope-Service/Starter/app.js
+++ Code-The-Scope-Service/Starter/app.js
@@ -3,18 +3,13 @@ var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 myApp.controller('mainController', ['$scope', '$timeout', '$filter', function($scope, $timeout, $filter) {
 
   $scope.handle = "";
+
   $scope.lowercasehandle = function() {
      return $filter('lowercase')($scope.handle);
    };
 
-   $scope.$watch('handle', function(newVal, oldVal) {
-     console.log(newVal, oldVal);
-   });
+   $scope.characters = 5;
 
-
-  // $timeout(function() {
-  //   $scope.name = 'Everybody';
-  // },3000);
 }]);
 //
 // var searchPeople = function(firstName, lastName, height, age, occupation) {
